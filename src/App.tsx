33469804552dc4c9aafd4bec7f037988bf7576a5
Forrest VanderBorgh <forrest.vanderborgh@dws.la>
2022-05-19 12:41:13 -0700

    using weather data

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ const reducer = (state: any, action: { type: any; payload: any }) => {
 
 const action = {
   getLaunches: (dispatch: (arg0: { type: string; payload: any }) => void) => {
-    fetch('https://api.spacexdata.com/v4/rockets')
+    fetch('https://api.weather.gov/gridpoints/LWX/89,70/forecast/hourly')
       .then((res) => {
         dispatch({ type: actionType.GET_LAUNCHES_LOADING, payload: {} });
         return res.json();
@@ -54,21 +54,81 @@ const action = {
   },
 };
 
+// data: {
+//   temperatures: [
+//     70, 90, 80
+//   ],
+//   windSpeed: [
+//     5, 10, 20
+//   ],
+//   windDirection: [
+//     'N', 'NE', 'E'
+//   ],
+//   shortForecast: ['sunny']
+// }
+
 const handle = {
   launchResponse: (response: any) => {
-    const shapedData = response.map((item: any) => {
+    const {
+      properties: { periods: rawPeriods },
+    } = response;
+
+    const periods = rawPeriods.slice(0, 24);
+
+    const temperatures = periods.map((item: any) => {
       return {
-        id: item.id,
-        x: item.name,
-        y: item.success_rate_pct,
+        x: item.endTime,
+        y: item.temperature,
       };
     });
+
+    const windSpeed = periods.map((item: any) => {
+      return {
+        x: item.endTime,
+        y: item.windSpeed,
+      };
+    });
+
+    const tileData = periods.map((item: any) => {
+      console.log('item', item);
+      return {
+        endTime: item.endTime,
+        icon: item.icon,
+        windDirection: item.windDirection,
+        shortForecast: item.shortForecast,
+      };
+    });
+
+    const shapedData = {
+      temperatures,
+      windSpeed,
+      tileData,
+    };
     return shapedData;
   },
 };
 
+const WeatherTile = (
+  hour: string,
+  image: string,
+  windDirection: string,
+  shortForecast: string
+) => {
+  console.log('hour', hour);
+  return (
+    <div>
+      <p>{hour}</p>
+      <img src={image} />
+      <p>{windDirection}</p>
+      <p>{shortForecast}</p>
+    </div>
+  );
+};
+
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  console.log('STATE: ', state);
 
   return (
     <div className="App">
@@ -80,21 +140,52 @@ function App() {
         >
           Get Launches
         </button>
-        {state.data && state.data.length > 0 && (
-          <VictoryChart
-            theme={VictoryTheme.material}
-            domainPadding={10}
-            height={200}
-          >
-            <VictoryBar
-              style={{ data: { fill: '#c43a31' } }}
-              data={state.data}
-              categories={{
-                x: state.data.map((item: { x: string }) => item.x),
-              }}
-            />
-          </VictoryChart>
-        )}
+        {state.data &&
+          state.data.temperatures &&
+          state.data.temperatures.length > 0 && (
+            <>
+              <VictoryChart
+                theme={VictoryTheme.material}
+                domainPadding={10}
+                height={200}
+              >
+                <VictoryBar
+                  style={{ data: { fill: '#c43a31' } }}
+                  data={state.data.temperatures}
+                  categories={{
+                    x: state.data.temperatures.map((item: { x: string }) =>
+                      new Date(item.x).getHours()
+                    ),
+                  }}
+                />
+              </VictoryChart>
+              <VictoryChart
+                theme={VictoryTheme.material}
+                domainPadding={10}
+                height={200}
+              >
+                <VictoryBar
+                  style={{ data: { fill: '#c43a31' } }}
+                  data={state.data.windSpeed}
+                  categories={{
+                    x: state.data.windSpeed.map((item: { x: string }) =>
+                      new Date(item.x).getHours()
+                    ),
+                  }}
+                />
+              </VictoryChart>
+              <div className="tile-parent">
+                {state.data.tileData.map((item: any, index: number) => {
+                  return WeatherTile(
+                    item.endTime,
+                    item.icon,
+                    item.windDirection,
+                    item.shortForecast
+                  );
+                })}
+              </div>
+            </>
+          )}
       </header>
     </div>
   );
